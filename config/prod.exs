67f70af57996d53856b779bed041e036c0f6d612
Marten Klitzke <me@mortik.xyz>
2016-04-17 16:26:39 +0200

    first commit

---
+++ config/prod.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+config :app, App.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "apresentfor.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :app, App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20,
+  ssl: true
